4a253f00cb32c3b6f30419753d2a2c27bb654061
Raj Aryan Dash <rajaryanrad@gmail.com>
2022-09-27 22:10:46 +0530

    Update to graphics and classes\n Create the following classes:     * Snake     * Fruit Changes on Snake:     * Add graphics     * Add logic for drawing the Snake     * Add logic for movement     * Add logic for reset Fruit:     * Create class     * Add drawing     * Miss loading in the graphics for the Fruit     * Randomise positon Changes on Main:     * Drawing elements of snake, fruit and the board     * Check for fail and collision     * Game over function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
+import random
 from pygame.math import Vector2
+
 
 class Snake():
     def __init__(self):
@@ -8,13 +10,121 @@ class Snake():
         self.direction = Vector2(0, 0)
         self.new_block = False
 
+        self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
+
+        self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
+
+        self.body_tr = pygame.image.load('Graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
+
+        self.crunch_sound = pygame.mixer.Sound('Sound/Crunch.wav')
+
+    def draw_snake(self):
+        self.update_head_graphics()
+        self.update_tail_graphics()
+        for index, block in enumerate(self.body):
+            snake_rect = pygame.Rect(int(block.x) * cell_size, int(block.y) * cell_size, cell_size, cell_size)
+
+            if index == 0:
+                screen.blit(self.head, snake_rect)
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, snake_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, snake_rect)
+                elif previous_block.y == next_block.x:
+                    screen.blit(self.body_horizontal, snake_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or next_block.x == -1 and previous_block.y == -1:
+                        screen.blit(self.body_tl, snake_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or next_block.x == 1 and previous_block.y == -1:
+                        screen.blit(self.body_tr, snake_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or next_block.x == 1 and previous_block.y == 1:
+                        screen.blit(self.body_br, snake_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or next_block.x == -1 and previous_block.y == 1:
+                        screen.blit(self.body_bl, snake_rect)
+
+    def update_head_graphics(self):
+        head_orientation = self.body[1] - self.body[0]
+        if head_orientation == Vector2(1, 0):
+            self.head = self.head_left
+        elif head_orientation == Vector2(-1, 0):
+            self.head = self.head_right
+        elif head_orientation == Vector2(0, 1):
+            self.head = self.head_down
+        elif head_orientation == Vector2(0, -1):
+            self.head = self.head_up
+
+    def update_tail_graphics(self):
+        tail_orientation = self.body[-1] - self.body[-2]
+        if tail_orientation == Vector2(1, 0):
+            self.tail = self.tail_right
+        elif tail_orientation == Vector2(-1, 0):
+            self.tail = self.tail_left
+        elif tail_orientation == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif tail_orientation == Vector2(0, -1):
+            self.tail = self.tail_down
+
+    def move_snake(self):
+        if self.new_block:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0,body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+        def add_block(self):
+            self.new_block = True
+
+        def play_crunch_sound(self):
+            self.crunch_sound.play()
+
+        def reset(self):
+            self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
+            self.direction = Vector2(0, 0)
+
+
+class Fruit():
+    def __init__(self):
+        self.randomize()
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x)*cell_size, int(self.pos.y)*cell_size, cell_size, cell_size)
+        screen.blit(apple,fruit_rect)
+
+    def randomize(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
 
 class Main():
     def __init__(self):
         self.snake = Snake()
+        self.fruit = Fruit()
 
     def draw_elements(self):
         self.draw_grass()
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
 
     def draw_grass(self):
         global grass_colour
@@ -30,12 +140,32 @@ class Main():
                         grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
                         pygame.draw.rect(screen, grass_colour, grass_rect)
 
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            self.fruit.randomize()
+            self.snake.add_block()
+            self.snake.play_crunch_sound()
+
+        for block in self.snake.body[1:]:
+            if block == self.fruit.pos:
+                self.fruit.randomize()
+
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+
+        for block in self.snake.body[1:]:
+            if self.snake.body[0] == block:
+                self.game_over()
+
+    def game_over(self):
+        self.snake.reset()
+
 
 # initialising pygame
 pygame.init()
 clock = pygame.time.Clock()
 FPS = 60
-main_game = Main()
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 100)
 
@@ -49,6 +179,9 @@ pygame.display.set_caption("Snake")
 lime = (175, 215, 70)
 grass_colour = (167, 209, 61)
 
+# main game
+main_game = Main()
+
 # main loop
 while True:
     for event in pygame.event.get():
@@ -56,8 +189,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == SCREEN_UPDATE:
-            pass
 
     # drawing
     screen.fill(lime)
